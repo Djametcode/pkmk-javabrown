2f2f924ee757467d5d16d88eb4c813c2c910fd74
Djamet Code <kampretsiji1@gmail.com>
2024-07-07 00:16:57 +0700

    simple update

--- pkmk-java/src/App.jsx
+++ pkmk-java/src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
         {/* <div className=" absolute top-0 right-0 z-0">
           <div className=" bg-[#ff9d02] h-[800px] w-[900px] rounded-bl-[500px]"></div>
         </div> */}
-        <div className=" w-full relative pr-36 pl-36 ">
+        <div className=" w-full relative pr-44 pl-44 ">
           <NavbarComponent />
           <HeroComponent />
           <AboutUsComponent />
